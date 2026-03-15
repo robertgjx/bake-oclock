fd367d3c7cd7e9328b57c5e364978459ee035332
Roberto Gonzalez Jr <robertogonzalezjr@MacBookAir.lan>
2026-03-15 01:52:10 -0500

    next.config.mjs fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.supabase.co https://*.supabase.com",
-          },
-        ],
-      },
-    ]
-  },
-}
+const nextConfig = {}
 
 export default nextConfig
